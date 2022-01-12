35d37d0bcd9a56adc12e6d5234900809d2da2887
BaghirzadeMamedik <brizeforge@gmail.com>
2022-01-12 18:13:35 +0400

    Add project files.

---
+++ test/Program.cs
@@ -0,0 +1,47 @@
+﻿Console.Write("birinci reqemi daxil edin: ");
+string sreqem = Console.ReadLine();
+
+Console.Write("ikinci reqemi daxil edin: ");
+
+string sreqem2 = Console.ReadLine();
+
+int reqem = Convert.ToInt32(sreqem);
+
+int reqem2 = Convert.ToInt32(sreqem2);
+
+Console.Write("Emeli daxil edin: ");
+
+string emel = Console.ReadLine();
+
+decimal total;
+
+if (emel == "+")
+{
+    total = reqem + reqem2;
+    Console.WriteLine(total);
+}
+else if (emel == "-")
+{
+    total = reqem - reqem2;
+    Console.WriteLine(total);
+}
+else if (emel == "*")
+{
+    total = reqem * reqem2;
+    Console.WriteLine(total);
+}
+else if (emel == "/")
+{
+    total = reqem / reqem2;
+    Console.WriteLine(total);
+}
+else
+{
+    Console.WriteLine("duzgun emeli daxil edin!!!");
+}
+
+
+
+
+
+
